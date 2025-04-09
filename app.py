07c006f8abac4b28dc2cac73e07c7eaf0ae83de6
SalmaanShire365 <salmaanshire@gmail.com>
2025-04-09 01:05:48 -0500

    Added flask web app entry + config.py for api manipulation

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 # Entry point of the Flask Application
 # Define routes that will handle incoming HTTP requests from frontend
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Saving Money'
 
--- config.py
+++ config.py
@@ -1,2 +1,15 @@
-# Store API key/ config data
-# Store the API URL in config.py
+import http.client
+
+conn = http.client.HTTPSConnection("facebook-pages-scraper2.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "6bbf527987msh805263045201c3bp196394jsn709dd888d6b7",
+    'x-rapidapi-host': "facebook-pages-scraper2.p.rapidapi.com"
+}
+
+conn.request("GET", "/get_facebook_reels_details?link=https%3A%2F%2Fwww.facebook.com%2Fmancity", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
